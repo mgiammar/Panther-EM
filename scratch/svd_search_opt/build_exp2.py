51749352b129ceef547e6f120ba7f6ee8625d91f
mgiammar <mdgiammar@gmail.com>
2026-09-22 10:47:12 -0700

    chore(search): clean up scratch development files

--- scratch/svd_search_opt/build_exp2.py
+++
@@ -1,2 +0,0 @@
-import sys; sys.path.insert(0,"scratch/svd_search_opt/kernels"); import exp_loader
-m=exp_loader.load(name="panther_exp2_irfft_stats", src="irfft_stats_exp2.cu"); print("exp2 built:", m.get_supported_configs()[:3], "...")
--- scratch/svd_search_opt/run_baselines.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/mgiammar/git_repositories/Panther-EM
-until grep -q 'exit128=' scratch/svd_search_opt/validate_real3.log; do sleep 2; done
-export TORCH_EXTENSIONS_DIR=$PWD/scratch/svd_search_opt/torch_ext_dev
-L=scratch/svd_search_opt/validate_real4.log
-: > $L
-timeout 3000 /home/mgiammar/miniconda3/envs/panther-em-dev/bin/python scratch/svd_search_opt/validate_real.py --num-particles 4 --num-hyps 2048 --n-psis 256 --pixel-batch 512 --hyp-batch 512 --repeats 2 >> $L 2>&1
-echo "exit_base=$?" >> $L
